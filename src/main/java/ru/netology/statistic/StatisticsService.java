7f78e4bcdf2be740e9a0f6f0014035eaf8d4f9fd
tajikei <k.majita.00@gmail.com>
2023-09-06 16:44:48 +0900

    但馬修正

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -38,6 +38,7 @@ public class StatisticsService {
 
   private static String fetchRemoteObject(String location) throws Exception {
     URL url = new URL(location);
+    
     URLConnection connection = url.openConnection();
     BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
     String body = reader.lines().collect(Collectors.joining());
