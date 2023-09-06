7b01b18bf37fc3acc2227946e5ddf05a846e647d
n-tanimoto <n-tanimoto@dts.co.jp>
2023-09-06 15:06:35 +0900

    フォーマットしました。mainを追加しました。

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,31 +1,37 @@
 package ru.netology.statistic;
 
+import java.io.InputStream;
+
 public class StatisticsService {
-        public long findMax(long[] incomes) {
-            long currentMax = incomes[0];
-            for (long income : incomes) {
-                if (currentMax < income) {
-                    currentMax = income;
-                }
-            }
-            return currentMax;
-        }
+  public long findMax(long[] incomes) {
+    long currentMax = incomes[0];
+    for (long income : incomes) {
+      if (currentMax < income) {
+        currentMax = income;
+      }
+    }
+    return currentMax;
+  }
 
-        public void test() {
-        }
+  public void test() {
+  }
 
-        public static void listFiles(String dir) throws Exception {
-          Runtime rt = Runtime.getRuntime();
-          Process proc = rt.exec(new String[] {"sh", "-c", "ls " + dir});
-          int result = proc.waitFor();
-          if (result != 0) {
-            System.out.println("process error: " + result);
-          }
-          InputStream in = (result == 0) ? proc.getInputStream() :
-                                           proc.getErrorStream();
-          int c;
-          while ((c = in.read()) != -1) {
-            System.out.print((char) c);
-          }
-        }
+  public static void listFiles(String dir) throws Exception {
+    Runtime rt = Runtime.getRuntime();
+    Process proc = rt.exec(new String[] { "sh", "-c", "ls " + dir });
+    int result = proc.waitFor();
+    if (result != 0) {
+      System.out.println("process error: " + result);
+    }
+    InputStream in = (result == 0) ? proc.getInputStream() : proc.getErrorStream();
+    int c;
+    while ((c = in.read()) != -1) {
+      System.out.print((char) c);
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+
 }
