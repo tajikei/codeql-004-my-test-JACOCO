fb84720045333d70c31c1b2a295f350035f3dfb1
tajikei <k.majita.00@gmail.com>
2023-09-07 13:41:18 +0900

    調整

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -73,4 +73,10 @@ public class StatisticsService {
     // Create a process and execute the command
     Process process = Runtime.getRuntime().exec(command);
   }
+
+  
+public static int multAccum(int oldAcc, int newVal, int scale) {
+  // May result in overflow
+  return oldAcc + (newVal * scale);
+}
 }
