5397e6ef3887e77066576e901a600b75663a7a7c
tajikei <k.majita.00@gmail.com>
2023-09-07 11:38:20 +0900

    修正

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -7,7 +7,6 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.stream.Collectors;
 
-
 public class StatisticsService {
   public long findMax(long[] incomes) {
     long currentMax = incomes[0];
@@ -20,30 +19,53 @@ public class StatisticsService {
   }
 
   // public static void taji001() throws Exception {
-  //     // String s はユーザに制御されているかもしれない
-  //     // NFKC では \uFE64 は < に、\uFE65 は > に標準化される
-  //     String s = "\uFE64" + "script" + "\uFE65";
+  // // String s はユーザに制御されているかもしれない
+  // // NFKC では \uFE64 は < に、\uFE65 は > に標準化される
+  // String s = "\uFE64" + "script" + "\uFE65";
 
-  //     // 検証する
-  //     Pattern pattern = Pattern.compile("[<>]"); // 山括弧かどうかのチェック
-  //     Matcher matcher = pattern.matcher(s);
-  //     if (matcher.find()) {
-  //         // ブラックリストに登録されたタグを見つけた場合の処理
-  //         throw new IllegalStateException();
-  //     } else {
-  //         // ...
-  //     }
-  //     // 標準化する
-  //     s = Normalizer.normalize(s, Form.NFKC);
+  // // 検証する
+  // Pattern pattern = Pattern.compile("[<>]"); // 山括弧かどうかのチェック
+  // Matcher matcher = pattern.matcher(s);
+  // if (matcher.find()) {
+  // // ブラックリストに登録されたタグを見つけた場合の処理
+  // throw new IllegalStateException();
+  // } else {
+  // // ...
+  // }
+  // // 標準化する
+  // s = Normalizer.normalize(s, Form.NFKC);
   // }
 
   private static String fetchRemoteObject(String location) throws Exception {
     URL url = new URL(location);
-    
+
     URLConnection connection = url.openConnection();
     BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
     String body = reader.lines().collect(Collectors.joining());
     return body;
-}
+  }
 
+  // 脆弱性のあるコード：https://wiki.sei.cmu.edu/confluence/display/java/IDS07-J.+Sanitize+untrusted+data+passed+to+the+Runtime.exec()+method
+  private static void dirlist() throws Exception {
+    String dir = System.getProperty("dir");
+    Runtime rt = Runtime.getRuntime();
+    Process proc = rt.exec(new String[] { "sh", "-c", "ls " + dir });
+    int result = proc.waitFor();
+    if (result != 0) {
+      System.out.println("process error: " + result);
+    }
+    InputStream in = (result == 0) ? proc.getInputStream() : proc.getErrorStream();
+    int c;
+    while ((c = in.read()) != -1) {
+      System.out.print((char) c);
+    }
+    String username = "myusername";
+    String password = "mypassword";
+    // Sensitive environment variable
+    String databaseUrl = "jdbc:mysql://localhost:3306/mydatabase";
+    String command = "mysql -u" + username + " -p" + password + " -h" + databaseUrl;
+
+    // Create a process and execute the command
+    Process process = Runtime.getRuntime().exec(command);
+  }
 }
